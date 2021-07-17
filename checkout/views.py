52ef7fd4d638869238f5b310d9f53a8687bc66fe
glanville49@googlemail.com <glanville49@googlemail.com>
2021-07-17 15:34:23 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -11,8 +11,10 @@ def checkout(request):
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
-    context = [
+    context = {
         'order_form': order_form,
-    ]
+        'strip_public_key': 'pk_test_51JEEjOHf5taf5Ecwfi2go5EHQPyhpsVbRsB233hT2BQG8ExVbIUblZcBrA5faWfS0bLERAMiFv2B4K1Igx80uQj800PQV9HdmK',
+        'client_secret': 'test client secret',
+    }
 
     return render(request, template, context)
